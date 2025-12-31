db84b2ae16f499c4524ebe501d2162ef55accec3
Richard <1866700+rs3201@users.noreply.github.com>
2025-12-31 22:34:52 +0800

    Update Python dependencies to Python 3

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -570,7 +570,7 @@ install_dependencies(){
         done
     elif check_sys packageManager apt; then
         apt_depends=(
-            gettext build-essential unzip gzip python python-dev python-setuptools curl openssl libssl-dev
+            gettext build-essential unzip gzip python3 python3-dev python3-setuptools curl openssl libssl-dev
             autoconf automake libtool gcc make perl cpio libpcre3 libpcre3-dev zlib1g-dev libev-dev libc-ares-dev git qrencode
         )
 
